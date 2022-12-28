b3d890396e2a0b7a08bcace9da2faef750449d33
hacker228973 <kodiy9@gmail.com>
2022-12-28 12:45:17 +0200

    Initial commit

---
+++ src/HwOne.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class HwOne {
+    public static void main(String[] args) {
+
+        Scanner scanner= new Scanner(System.in);
+        System.out.println("pleas enter a variable a");
+        int a = scanner.nextInt();
+        System.out.println("pleas enter a variable b");
+        int b = scanner.nextInt();
+        System.out.println("a = "+a+"\n"+"b = "+b+"\n");
+
+        System.out.println("swap places...");
+        int c = b;
+        b = a;
+        a = c;
+        System.out.println("a = "+a+"\n"+"b = "+b);
+    }
+}
