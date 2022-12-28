49e7434f94a8794163952eac8de07d64790f8d20
hacker228973 <kodiy9@gmail.com>
2022-12-28 13:00:32 +0200

    lesson1

--- src/lesson2/HwOne.java
+++ src/lesson2/HwOne.java
@@ -1,3 +1,5 @@
+package lesson2;
+
 import java.util.Scanner;
 
 public class HwOne {
---
+++ src/lesson2/HwTwo.java
@@ -0,0 +1,9 @@
+package lesson2;
+
+public class HwTwo {
+    public static void main(String[] args) {
+        double latitude = 25.197195698959778;
+        double longitude = 55.27438397254892;
+        System.out.println("The tower named Burj Khalifa is located at the coordinates '"+latitude+" "+longitude+"'");
+    }
+}
